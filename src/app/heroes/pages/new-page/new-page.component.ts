38c4963f597fd33e7e2af7019cc89fab19b24561
Andres Jimenez <andres.jimenez@pragma.com.co>
2023-05-05 15:51:52 -0500

    add new hero form basic structure

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
   styles: [],
 })
-export class NewPageComponent {}
+export class NewPageComponent {
+  public publishers = [
+    { id: 'DC Comics', value: 'DC - Comics' },
+    { id: 'Marvel Comics', value: 'Marvel - Comics' },
+  ];
+}
